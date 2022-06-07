be6e8d20e5a27f2356b98c401faa933d43fd30af
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2022-06-06 22:22:05 -0300

    Aula 03 - Atividade 05: Aceitando apenas dias úteis

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -20,10 +20,14 @@ export class NegociacaoController {
 
 	public adiciona(): void {
 		const negociacao = this.criaNegociacao();
-		this.negociacoes.adiciona(negociacao);
+		if (negociacao.data.getDay() > 0 && negociacao.data.getDay() < 6) {
+			this.negociacoes.adiciona(negociacao);
 
-		this.atualizaView();
-		this.limparFormulario();
+			this.atualizaView();
+			this.limparFormulario();
+		} else {
+			this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
+		}
 	}
 
 	private criaNegociacao(): Negociacao {
