0c4b5e2d28fd71b4352249cd5b73bdd43af25722
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2022-06-07 16:24:43 -0300

    Aula 04 - Atividade 02: Isolando a lógica de conversão de entrada

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -12,4 +12,12 @@ export class Negociacao {
   get data(): Date {
     return new Date(this._data.getTime());
   }
+
+  public criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+    const exp = /-/g;
+		const data = new Date(dataString.replace(exp, ','));
+		const quantidade = parseInt(quantidadeString);
+		const valor = parseFloat(valorString);
+		return new Negociacao(data, quantidade, valor);
+  }
 }
